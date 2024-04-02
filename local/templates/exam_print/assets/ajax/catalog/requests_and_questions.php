30feb894a4ee17a6088f9fca652f22cd1beb76d5
david <davosar@inbox.ru>
2024-04-02 17:03:00 +0300

    add resize image func for 2 components(catalog and slider)

--- local/templates/exam_print/assets/ajax/catalog/requests_and_questions.php
+++ local/templates/exam_print/assets/ajax/catalog/requests_and_questions.php
@@ -1,5 +1,4 @@
 <?php
-
 use Bitrix\Main\Application;
 use Bitrix\Main\Context;
 use Bitrix\Main\Request;
@@ -7,23 +6,41 @@ use Bitrix\Main\Server;
 use Bitrix\Main\Loader;
 
 require_once $_SERVER['DOCUMENT_ROOT'] . '/bitrix/modules/main/include/prolog_before.php';
-
-header('Content-Type: application/json; windows-1251');
-
+header('Content-Type: application/json;  charset=UTF-8');
 Loader::includeModule('iblock');
 
 $jsonData = file_get_contents('php://input');
-$jsonData = htmlspecialchars($jsonData);
+$jsonData1 = htmlspecialchars($jsonData);
+$jsonData = json_decode($jsonData , true);
+
+if (!preg_match($phoneRegex, $jsonData['Phone'])) {
+    $errors[] = 'Неверный формат номера телефона';
+}
+
+if (!preg_match($emailRegex, $jsonData['Email'])) {
+    $errors[] = 'Неверный формат электронной почты';
+}
+
+if (!preg_match($nameRegex, $jsonData['Name'])) {
+    $errors[] = 'Неверный формат имени';
+}
+
+if (!empty($errors)) {
+    echo json_encode(['errors' => $errors]);
+    exit;
+}
 
 $el = new CIBlockElement;
 
 $arLoadProductArray = array(
     "IBLOCK_ID"         => 9,
     "NAME" => "Данные_из_формы_обращения",
-    "PREVIEW_TEXT"      => $jsonData,
+    "PREVIEW_TEXT"      => $jsonData1,
 );
+
 if ($PRODUCT_ID = $el->Add($arLoadProductArray)) {
     echo "New ID: ".$PRODUCT_ID;
-}
-else
+} else {
     echo "Error: ".$el->LAST_ERROR;
+}
+
--- local/templates/exam_print/catalog_detail_input_requests.js
+++ local/templates/exam_print/catalog_detail_input_requests.js
@@ -1,42 +1,85 @@
 
-const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-const phoneRegex = /^\+\d{1,2} \(\d{3}\) \d{3}-\d{2}-\d{2}$/;
-const nameRegex = /^[А-Яа-яЁё\s]+$/;
+// const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+// const phoneRegex = /^\+\d{1,2} \(\d{3}\) \d{3}-\d{2}-\d{2}$/;
+// const nameRegex = /^[А-Яа-яЁё\s]+$/;
+//     document.querySelector('.partners-requisites__form').addEventListener('submit', function(event) {
+//             event.preventDefault();
+//             let formData = new FormData(this);
+//
+//             let convertedData = {};
+//
+//             for (const [key, value] of formData.entries()) {
+//                 convertedData[key] = value;
+//             }
+    //     let jsonData = JSON.stringify(convertedData);
+    //     let xhr = new XMLHttpRequest();
+    //     xhr.open('POST', '/local/templates/exam_print/assets/ajax/catalog/requests_and_questions.php', true);
+    //     xhr.setRequestHeader('Content-Type', 'application/json; windows-1251');
+    //     xhr.onload = function() {
+    //     if (xhr.status === 200) {
+    //         console.log(jsonData);
+    //     } else {
+    //         console.log('Произошла ошибка при отправке формы: ' + xhr.status);
+    //     }
+    // };
+    //
+    // let errorMessage = '';
+    //
+    // if (!emailRegex.test(document.getElementById('EmailID').value)) {
+    //     errorMessage += 'Неверный формат электронной почты\n';
+    // } if (!phoneRegex.test(document.getElementById('PhoneID').value)) {
+    //     errorMessage += 'Неверный формат телефона\n';
+    // } if (!nameRegex.test(document.getElementById('NameID').value)) {
+    //     errorMessage += 'Неверный формат имени\n';
+    // }
+    // if (errorMessage === '') {
+    //     xhr.send(jsonData);
+    // } else {
+    //     alert(errorMessage);
+    // }
+
+//
+//
+// if (typeof BX == 'undefined') {
+//     var BX = {};
+// }
+//
+// if (typeof BX.ajax == 'undefined') {
+//     BX.ajax = {};
+// }
+//
+// (function() {
+//     var script = document.createElement('script');
+//     script.src = '/bitrix/js/main/core/core.js';
+//     document.head.appendChild(script);
+//
+//     script.onload = function() {
+//         BX.ajax.run();
+//     };
+// })();
+
 document.querySelector('.partners-requisites__form').addEventListener('submit', function(event) {
-        event.preventDefault();
-        let formData = new FormData(this);
-        let convertedData = {};
+    event.preventDefault();
+    let formData = new FormData(this);
+    let convertedData = {};
 
-        for (const [key, value] of formData.entries()) {
-            convertedData[key] = value;
-        }
-
-        let jsonData = JSON.stringify(convertedData);
-        let xhr = new XMLHttpRequest();
-        xhr.open('POST', '/local/templates/exam_print/assets/ajax/catalog/requests_and_questions.php', true);
-        xhr.setRequestHeader('Content-Type', 'application/json; windows-1251');
-        xhr.onload = function() {
-        if (xhr.status === 200) {
-            console.log(jsonData);
-        } else {
-            console.log('Произошла ошибка при отправке формы: ' + xhr.status);
-        }
-    };
-
-    let errorMessage = '';
-
-    if (!emailRegex.test(document.getElementById('EmailID').value)) {
-        errorMessage += 'Неверный формат электронной почты\n';
-    } if (!phoneRegex.test(document.getElementById('PhoneID').value)) {
-        errorMessage += 'Неверный формат телефона\n';
-    } if (!nameRegex.test(document.getElementById('NameID').value)) {
-        errorMessage += 'Неверный формат имени\n';
+    for (const [key, value] of formData.entries()) {
+        convertedData[key] = value;
     }
-    if (errorMessage === '') {
-        xhr.send(jsonData);
-    } else {
-        alert(errorMessage);
-    }
+
+    let jsonData = JSON.stringify(convertedData);
+
+    BX.ajax({
+        url: '/local/templates/exam_print/assets/ajax/catalog/requests_and_questions.php',
+        data: jsonData,
+        method: 'POST',
+        dataType: 'json',
+        headers: {'Content-Type': 'application/json; charset=UTF-8'},
+        onsuccess: function (data) {
+            if ('errors' in data) {
+                alert(data.errors.join('\n'));
+            }
+        }
+    });
+
 });
-
-
