89e81c5a0f6a5ea539544d4b68456ada8c74a667
david <davosar@inbox.ru>
2024-04-10 17:40:11 +0300

    add resize image func for 2 components(catalog and slider)

--- local/templates/exam_print/assets/ajax/ex/req.php
+++ local/templates/exam_print/assets/ajax/ex/req.php
@@ -12,7 +12,7 @@ $code = rand(1,1);
 $jsonData = file_get_contents('php://input');
 if (!empty($jsonData['phone'])) {
     $phone = urlencode($jsonData['phone']);
-    $url = "https://sms.ru/sms/send?api_id=3E609D03-FB45-0619-7B2D-ECAA6A08D59A&to=" . $jsonData$url = "https://sms.ru/sms/send?api_id=3E609D03-FB45-0619-7B2D-ECAA6A08D59A&to=" . $jsonData['phone'] . "&text=Ваш код подтверждения: " . $code; . "&text=Ваш код подтверждения: " . $code;
+    $url = "https://sms.ru/sms/send?api_id=3E609D03-FB45-0619-7B2D-ECAA6A08D59A&to=" . $jsonData$url = "https://sms.ru/sms/send?api_id=3E609D03-FB45-0619-7B2D-ECAA6A08D59A&to=" . $jsonData['phone'] . "&text=Ваш код подтверждения: " . $code;
     echo file_get_contents($url);
 }
 $confirmationCode = $_POST['confirmation_code'];
